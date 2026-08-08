3ae487bbf7f4a59d231dec177507dc4b407135d3
joel <joelqui15@hotmail.com>
2026-08-07 22:50:35 -0400

    Feat: created header and set up anchor links

---
+++ src/components/app/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "../header/Header.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="page">
+        <Header />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <>
+      <header className="header" id="home">
+        <img
+          src="src/images/logobrewd.png"
+          alt="logo of a coffee cup and text that say brew d'licious"
+          className="header__logo"
+        />
+        <div className="header__links">
+          <a href="#" className="header__link">
+            Home
+          </a>
+          <a href="#" className="header__link">
+            Menu
+          </a>
+          <a href="#" className="header__link">
+            Gallery
+          </a>
+          <a href="#" className="header__link">
+            Visit-us
+          </a>
+          <a href="#" className="header__link">
+            Contact
+          </a>
+        </div>
+        <button className="header__menu-button">View Menu</button>
+      </header>
+    </>
+  );
+}
+
+export default Header;
